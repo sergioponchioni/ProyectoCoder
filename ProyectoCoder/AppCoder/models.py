28e140585bce66f8233331eec4f3b93b0b1e67e7
sergioponchioni <ponchioni.sergio@gmail.com>
2021-12-16 19:11:47 -0300

    test

--- ProyectoCoder/AppCoder/models.py
+++ ProyectoCoder/AppCoder/models.py
@@ -37,11 +37,8 @@ class Estadio(models.Model):
     
     def __str__(self) :
         
-<<<<<<< HEAD
-        return f"NOMBRE {self.nombre} --- CAPACIDAD {self.capacidad} --- DIRECCION {self.direccion} --- ANIOFUNDACION {self.anioFundacion}"
-=======
         return f"NOMBRE {self.nombre} --- CAPACIDAD {self.capacidad} --- DIRECCION {self.direccion} --- ANIOFUNDACION {self.anioFundacion}"
         #Comentario
         #Otro Comentario de prueba
+        #otro mas
  
->>>>>>> d5b73a8902b914a406eed4f3432cab96cb4add02
